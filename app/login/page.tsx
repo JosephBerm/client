b78dc449ca5d9863892016a0f5a81e72079bbe57
Joseph Bermudez <42876560+JosephBerm@users.noreply.github.com>
2024-03-02 20:08:48 -0500

    login page
    basic styles and layout of the login page, fully responsive. No functionality yet but states were made.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,66 @@
+'use client'
+
 import React, { useState } from 'react'
 import InputTextBox from '@/src/components/InputTextBox'
+import '@/styles/login.css'
 
 const Page = () => {
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+
+	const handleEmailChange = (value: string) => {
+		setEmail(value)
+	}
+
+	const handlePasswordChange = (value: string) => {
+		setPassword(value)
+	}
+
+	const handleBlur = () => {
+		console.log('blurred')
+	}
+
+	const handleFocus = () => {
+		// Handle focus event if needed
+		console.log('focused')
+	}
+
+	const handleForgotPasswordClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+		e.preventDefault()
+		console.log('Forgot password clicked')
+	}
+
 	return (
-		<>
-			<InputTextBox />
-		</>
+		<div className='Login flex flex-col items-center justify-between absolute left-2/4 transform -translate-x-1/2 -translate-y-1/2'>
+			<h2 className='page-title'>Login</h2>
+			<fieldset className='min-h-96 flex flex-col gap-8 w-2/4 relative'>
+				<InputTextBox
+					type='text'
+					label='Email'
+					value={email}
+					handleChange={handleEmailChange}
+					handleBlur={handleBlur}
+					handleFocus={handleFocus}
+				/>
+
+				<InputTextBox
+					type='password'
+					label='Password'
+					value={password}
+					handleChange={handlePasswordChange}
+					handleBlur={handleBlur}
+					handleFocus={handleFocus}
+				/>
+
+				<div className='form-footer mt-10 flex flex-col items-center justify-center gap-10'>
+					<button className='submit'>Login</button>
+
+					<a href='#' className='forgot-password' onClick={handleForgotPasswordClick}>
+						Forgot Password?
+					</a>
+				</div>
+			</fieldset>
+		</div>
 	)
 }
 
--- src/components/InputTextBox.tsx
+++ src/components/InputTextBox.tsx
@@ -2,10 +2,29 @@
 
 import React from 'react'
 
-const InputTextBox = () => {
-	const [myState, setMyState] = React.useState(null)
+interface InputType {
+	type: string
+	value: string
+	label: string
+	handleChange: (value: string) => void
+	handleBlur: () => void
+	handleFocus: () => void
+}
 
-	return <div className='fafa'>InputTextBox</div>
+const InputTextBox: React.FC<InputType> = ({ type, label, value, handleChange, handleBlur, handleFocus }) => {
+	return (
+		<div className='InputTextBox flex flex-col'>
+			<label className='m-2'>{label}</label>
+			<input
+				type={type}
+				value={value}
+				onChange={(e) => handleChange(e.target.value)}
+				onBlur={handleBlur}
+				onFocus={handleFocus}
+				className='border-b border-gray-300'
+			/>
+		</div>
+	)
 }
 
 export default InputTextBox
