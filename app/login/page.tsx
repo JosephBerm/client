1505072a7aee08877d5fa5b5e8a08714810b10b6
Joseph Bermudez <42876560+JosephBerm@users.noreply.github.com>
2024-03-05 22:03:27 -0500

    proper error handling for the authentication

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -15,7 +15,7 @@ import '@/styles/login.css'
 const Page = () => {
 	const [credentials, setCredentials] = useState(new LoginCredentials())
 	const [isLoading, setIsLoading] = useState(false)
-	const route = useRouter()
+	const router = useRouter()
 
 	const handleChange = (key: keyof LoginCredentials, value: string) => {
 		setCredentials((prevCredentials) => ({
@@ -42,12 +42,16 @@ const Page = () => {
 		try {
 			setIsLoading(true)
 			const { data: authenticated } = await API.login(credentials)
-
-			if (authenticated) {
+			console.log('authenticated', authenticated)
+			if (authenticated.payload) {
 				const JWTToken = authenticated.payload
 				setCookie('at', JWTToken)
 				instance.defaults.headers.common['Authorization'] = `Bearer ${JWTToken}`
-				route.push('/dashboard')
+
+				//route to dashboard
+				router.push('/dashboard')
+			} else {
+				console.log(authenticated.message)
 			}
 		} catch (err) {
 			console.log(err)
@@ -56,8 +60,13 @@ const Page = () => {
 		}
 	}
 
+	const routeToSignUp = () => {
+		//route to sign up
+		router.push('/signup')
+	}
+
 	return (
-		<div className='Login flex flex-col items-center justify-between absolute left-2/4 transform -translate-x-1/2 -translate-y-1/2'>
+		<div className='Login'>
 			<h2 className='page-title'>Login</h2>
 			<form className='min-h-96 flex flex-col gap-8 w-2/4 relative' onSubmit={handleLogin}>
 				<InputTextBox
@@ -65,8 +74,6 @@ const Page = () => {
 					label='Email'
 					value={credentials.username}
 					handleChange={(value) => handleChange('username', value)}
-					handleBlur={handleBlur}
-					handleFocus={handleFocus}
 					autofocused={true}
 				/>
 
@@ -75,18 +82,22 @@ const Page = () => {
 					label='Password'
 					value={credentials.password}
 					handleChange={(value) => handleChange('password', value)}
-					handleBlur={handleBlur}
-					handleFocus={handleFocus}
 				/>
 
-				<div className='form-footer mt-10 flex flex-col items-center justify-center gap-10'>
+				<div className='form-footer flex flex-col items-center justify-center gap-10'>
+					<a className='clickable forgot-password mb-7' onClick={handleForgotPasswordClick}>
+						Forgot Password?
+					</a>
 					<button type='submit' className='submit'>
 						Login
 					</button>
 
-					<a href='#' className='forgot-password' onClick={handleForgotPasswordClick}>
-						Forgot Password?
-					</a>
+					<span className='button-subtitle'>
+						Don&apos;t have a password?&nbsp;
+						<a className='clickable inline-link' onClick={routeToSignUp}>
+							Sign up!
+						</a>
+					</span>
 				</div>
 			</form>
 		</div>
