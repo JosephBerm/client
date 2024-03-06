2680acd098cda29550c3dee62a21ff026841b835
Joseph Bermudez <42876560+JosephBerm@users.noreply.github.com>
2024-03-05 22:05:36 -0500

    Creating `SignupForm` Class

---
+++ src/classes/SignupForm.ts
@@ -0,0 +1,10 @@
+export default class SignupForm {
+	constructor(
+		public username: string = '',
+		public email: string = '',
+		public password: string = '',
+		public firstName: string = '',
+		public lastName: string = '',
+		public dateOfBirth?: Date
+	) {}
+}
