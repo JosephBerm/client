9dcca357be50529c2aaaca6c0ff4b664545b709c
Joseph Bermudez <42876560+JosephBerm@users.noreply.github.com>
2024-03-09 20:44:35 -0500

    created products inside store obj in api

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,11 +4,16 @@ import axios, { AxiosResponse, AxiosError } from 'axios'
 import LoginCredentials from '@/classes/LoginCredentials'
 import SignupForm from '@/classes/SignupForm'
 
-const httpService = new HttpService(baseInstance)
-
 const API = {
 	login: async (credentials: LoginCredentials) => await baseInstance.post<any>('/account/login', credentials),
-	signup: async (form: SignupForm) => await baseInstance.post<any>('/account/signup', form)
+	signup: async (form: SignupForm) => await baseInstance.post<any>('/account/signup', form),
+	store: {
+		products: {
+			getList: async <T>() => await HttpService.get<T>('/products'),
+			get: async <T>(productId: string) => await HttpService.get<T>(`/products/${productId}`),
+			create: async <T>(product: T) => await HttpService.post<T>(`/products`, product),
+		},
+	},
 }
 
 export default API
