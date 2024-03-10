9c8256eea5499299c595c18ec88c33a4f957a72c
Joseph Bermudez <42876560+JosephBerm@users.noreply.github.com>
2024-03-09 20:44:20 -0500

    ignore: format file

--- src/classes/Product.ts
+++ src/classes/Product.ts
@@ -1,17 +1,17 @@
 export class Product {
-    id: string | null = null;
-    sku: string = '';
-    name: string = '';
-    description: string = '';
-    price: number = 0;
-    image: string | null = null;
-    category: ProductsCategory | null = null;
+	id: string | null = null
+	sku: string = ''
+	name: string = ''
+	description: string = ''
+	price: number = 0
+	image: string | null = null
+	category: ProductsCategory | null = null
 
-    toString(): string {
-        return `Product: ${this.name} - ${this.description} - ${this.price} - ${this.image} - ${this.category}`;
-    }
+	toString(): string {
+		return `Product: ${this.name} - ${this.description} - ${this.price} - ${this.image} - ${this.category}`
+	}
 }
 
 export enum ProductsCategory {
-    ProductOne
+	ProductOne,
 }
