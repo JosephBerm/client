f0678ed4d5c6d27e1d2ec5e14dc50e5720add5a1
Joseph Bermudez <42876560+JosephBerm@users.noreply.github.com>
2024-03-09 21:08:43 -0500

    Making body work with fixed header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
@@ -20,7 +19,6 @@ export default function RootLayout({
 				{children}
 				<ToastContainer />
 			</body>
-		
 		</html>
 	)
 }
