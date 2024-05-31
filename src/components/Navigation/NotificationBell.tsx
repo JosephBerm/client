af950c5fd94b8912036693377bc4b51284496ca6
melrpm <medelorbe@outlook.com>
2024-05-30 21:58:54 -0400

    add notification data

--- src/components/Navigation/NotificationBell.tsx
+++ src/components/Navigation/NotificationBell.tsx
@@ -2,18 +2,20 @@ import React, { useState, useMemo, useEffect } from 'react'
 import classNames from 'classnames'
 import Notification from '@/src/classes/Notification'
 import FloatingMenu, { FloatingMenuProps } from '@/src/common/FloatingMenu'
+import { useAccountStore } from '@/src/stores/user'
 
 function NotificationBell() {
-	const [notifications, setNotifications] = useState<Notification[]>([])
+	const UserData = useAccountStore((state) => state.User)
+	const [notifications, setNotifications] = useState<Notification[]>(UserData.notifications || [])
 
 	const hasUnreadNotifications = useMemo(() => {
 		return notifications.some((notification) => !notification.read)
 	}, [notifications])
 
-	const [iconClass, setIconClass] = useState<string>(`fa-regular fa-bell${hasUnreadNotifications ? 'unread' : ''}`)
+	const [iconClass, setIconClass] = useState<string>(`fa-regular fa-bell ${hasUnreadNotifications ? 'unread' : ''}`)
 
 	useEffect(() => {
-		const newIconClass = `fa-regular fa-bell${hasUnreadNotifications ? 'unread' : ''}`
+		const newIconClass = `fa-regular fa-bell ${hasUnreadNotifications ? 'unread' : ''}`
 		setIconClass(newIconClass)
 	}, [notifications, hasUnreadNotifications]) // Include dependencies
 
