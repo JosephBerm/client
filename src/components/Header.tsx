d0a3f6e04e4082503747e0abca6516eb1c2e7870
Joseph Bermudez <42876560+JosephBerm@users.noreply.github.com>
2024-06-06 19:36:28 -0400

    if path doesn't start with internal app route, return public nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,7 @@ import PublicNavBar from '@/components/Navigation/PublicNavBar'
 import { AccountRole } from '@/classes/Enums'
 import { useAccountStore } from '@/src/stores/user'
 import { usePathname } from 'next/navigation'
+import Routes from '@/services/routes'
 
 function Header() {
 	const User = useAccountStore((state) => state.User)
@@ -16,6 +17,8 @@ function Header() {
 
 	// Set the navbar based on the role.
 	// We might want to render different navbars based on the user's location, or other factors.
+	if (!path.includes(Routes.InternalAppRoute)) return <PublicNavBar />
+
 	switch (User.role) {
 		case AccountRole.Customer:
 		case AccountRole.Admin:
